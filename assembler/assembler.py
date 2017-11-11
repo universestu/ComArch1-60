5a62606921c0d5333b3b258d40a77ce3b5bcc20c
darunphop <darunphop.p@gmail.com>
2017-11-11 19:44:38 +0700

    split data to pieces

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,3 +1,15 @@
 import input
 
+data = input.data
+# print(data[10][0:6])
 
+processed_data = []
+tmp = 0
+for head in data:
+    processed_data.append([head[0:6].rstrip()])
+    # print(head.split())
+    for sp in head[7:].split():
+        processed_data[tmp].append(sp)
+    tmp += 1
+for x in processed_data:
+    print(x)
--- assembler/input.py
+++ assembler/input.py
@@ -1,4 +1,4 @@
 import sys
 
 with open(sys.argv[1], 'r') as input_file:
-    data = filter(None, (line.strip() for line in input_file))
+    data = filter(None, (line.rstrip() for line in input_file))
