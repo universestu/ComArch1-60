4bf19d4aaffa69e55d92d894875590d41c6f60ab
chalitphol kuensuwan <chalitphol@gmail.com>
2017-11-13 18:15:41 +0700

    fix halt + printState

--- simulator.py
+++ simulator.py
@@ -3,6 +3,8 @@ import sys
 NUMMEMORY = 65536 # maximum number of words in memory
 NUMREGS = 8 # number of machine registers
 MAXLINELENGTH = 1000
+halt = 0
+count = 0
 
 class stateStruct:
     pc = None
@@ -40,6 +42,9 @@ def twosComplement(c): #convert 2's complement to Decimal
 		return int(c[10:25],2)
 
 def process(c,s): #check opcode and select fucntion
+    global count
+    printState(s)
+    count += 1
     if c[0:3] == "000": #check opcode add
         add(c,s)
     elif c[0:3] == "001": #check opcode nand
@@ -53,41 +58,62 @@ def process(c,s): #check opcode and select fucntion
     elif c[0:3] == "101": #check opcode jump
         jalr(c,s)
     elif c[0:3] == "110": #check opcode halt
-        halt(c,s)
+        halt(count,s)
     # if opcode is noop do nothing
 
 def add(c,s):
-    regA = int(c[4:6],2) #select register at regA
-    regB = int(c[7:9],2) #select register at regB
-    destReg = int(c[23:25],2) #select register at destReg
+    regA = int(c[3:6],2) #select register at regA
+    regB = int(c[6:9],2) #select register at regB
+    destReg = int(c[22:25],2) #select register at destReg
     s.reg[destReg] = s.reg[regA] + s.reg[regB] #store add of A and B
+    print(c)
+    print("add:regA = ",regA)
+    print("add:regB = ",regB)
+    print(s.reg[destReg])
     s.pc += 1 #next instruction
 
 def nand(c,s):
-    regA = int(c[4:6],2)
-    regB = int(c[7:9],2)
-    destReg = int(c[23:25],2)
+    regA = int(c[3:6],2)
+    regB = int(c[6:9],2)
+    destReg = int(c[22:25],2)
     s.reg[destReg] = ~(s.reg[regA] & s.reg[regB])
+    print(c)
+    print("nand:regA = ",regA)
+    print("nand:regB = ",regB)
+    print(s.reg[destReg])
     s.pc += 1
 
 def lw(c,s):
-    regA = int(c[4:6],2)
-    regB = int(c[7:9],2)
-    offsetField = twosComplement(c) #convert 2's complement string to decimal
+    regA = int(c[3:6],2)
+    regB = int(c[6:9],2)
+    print(c)
+    print("lw:regA = ",regA)
+    print("lw:regB = ",regB)
+    offsetField = twosComplement(c)
+    print("lw:offset = ",offsetField) #convert 2's complement string to deciamal
     s.reg[regB] = s.mem[s.reg[regA]+offsetField]
     s.pc += 1
 
 def sw(c,s):
-    regA = int(c[4:6],2)
-    regB = int(c[7:9],2)
+    regA = int(c[3:6],2)
+    regB = int(c[6:9],2)
     offsetField = twosComplement(c)
     s.mem[s.reg[regA]+offsetField] = s.reg[regB]
+    print(c)
+    print("sw:regA = ",regA)
+    print("sw:regB = ",regB)
+    print(s.reg[regB])
+    print(s.mem[s.reg[regA]+offsetField])
     s.pc += 1
 
 def beq(c,s):
-    regA = int(c[4:6],2)
-    regB = int(c[7:9],2)
+    regA = int(c[3:6],2)
+    regB = int(c[6:9],2)
     offsetField = twosComplement(c)
+    print(c)
+    print("beq:regA = ",regA)
+    print("beq:regB = ",regB)
+    print(offsetField)
     if s.reg[regA] == s.reg[regB]:
         s.pc = s.pc + 1 + offsetField
     else:
@@ -95,8 +121,8 @@ def beq(c,s):
 
 
 def jalr(c,s):
-    regA = int(c[4:6],2)
-    regB = int(c[7:9],2)
+    regA = int(c[3:6],2)
+    regB = int(c[6:9],2)
     if regA != regB:
         s.reg[regB] = s.pc + 1
         s.pc = s.reg[regA]
@@ -104,14 +130,18 @@ def jalr(c,s):
         s.reg[regB] = s.pc + 1
         s.pc = s.pc + 1
 
-def halt(c,s):
+def halt(cnt,s):
+    global halt
     s.pc += 1
-
+    print ("machine halted")
+    print ("total of ",cnt," instructions executed.")
+    print ("final state of machine:")
+    printState(s)
+    halt = 1
 
 def main():
     state = stateStruct()
-    count = 1
-    i = 0
+    global halt
     with open(sys.argv[1], 'r') as f: #open file
         try:
             for line in f:
@@ -119,16 +149,7 @@ def main():
                 state.numMemory += 1
         except IOError:
             print ("error: usage:",sys.argv[1],"<machine-code file>\n".format(e.errno, e.strerror))
-    printState(state)
-    while i != 1:
+    while halt != 1:
         process (convertToBinary(state.mem[state.pc]),state)
-        printState(state)
-        count += 1
-        if convertToBinary(state.mem[state.pc])[0:3] == "110":
-            i = 1
-    print ("machine halted")
-    print ("total of ",count," instructions executed.")
-    print ("final state of machine:")
-    printState(state)
 
 main()
