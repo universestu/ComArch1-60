8d5d3f874b194d89ade99a6c2e569e7f31fef35d
ICQCQ <darunphop.p@gmail.com>
2017-11-24 16:34:24 +0700

    add comment

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,28 +1,27 @@
 import input
 import instruction as inst
 
-data = input.data
+data = input.data                                       # Input file to Data memory
 
-processed_data = []
+processed_data = []                                     # Processed input Data memory
 tmp = 0
 for head in data:
-    processed_data.append([tmp])
-    processed_data[tmp].append([head[0:6].rstrip()])
-    for sp in head[7:].split():
+    processed_data.append([tmp])                        # Append Number of line
+    processed_data[tmp].append([head[0:6].rstrip()])    # Append label
+    for sp in head[7:].split():                         # Append fileds and comment part
         processed_data[tmp].append(sp)
     tmp += 1
 
-inst.setSymbolic(processed_data)
+inst.setSymbolic(processed_data)                        # Set Symbolic memory
 
 with open(input.sys.argv[1].split('.')[0] + '-out.asm', 'w') as output_file:
     for x in processed_data:
         try:
-            if('-b' in input.sys.argv):
-                print('write in binary mode')
+            if('-b' in input.sys.argv):                                     # Write output in binary mode
                 output_file.writelines(str(inst.seperate(x))  +'\n')
             else:
-                output_file.writelines(str(int(inst.seperate(x), 2))+'\n')
+                output_file.writelines(str(int(inst.seperate(x), 2))+'\n')  # Write output in decimal mode
         except TypeError:
-            output_file.writelines(str(inst.seperate(x)) +'\n')
+            output_file.writelines(str(inst.seperate(x)) +'\n')             # Write .fill part
     output_file.close()
 exit(0)
